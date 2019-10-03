ff23231e603ec7b25e90d48d133c8ea7d51e7b39
Nik Vogrinec <vogrinec.nik@gmail.com>
2019-10-03 19:23:05 +0200

    changed icons to one file

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,19 +1,17 @@
-const gulp = require("gulp"),
-    postcss = require("gulp-postcss"),
-    cssvars = require("postcss-simple-vars"),
-    nested = require("postcss-nested"),
-    autoprefixer = require("autoprefixer"),
-    importCss = require("postcss-import"),
-    mixins = require("postcss-mixins");
+var gulp = require('gulp'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
-gulp.task("style", function () {
-    return gulp.src("./app/assets/styles/style.css")
-        .pipe(postcss([
-            importCss, mixins, cssvars, nested, autoprefixer
-        ]))
-        .on("error", function (err) {
-            console.log(err.toString());
-            this.emit("end");
-        })
-        .pipe(gulp.dest("./app/temp/styles"));
+gulp.task('styles', function() {
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .on('error', function(errorInfo) {
+      console.log(errorInfo.toString());
+      this.emit('end');
+    })
+    .pipe(gulp.dest('./app/temp/styles'));
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,25 +1,27 @@
-const gulp = require("gulp"),
-    watch = require("gulp-watch"),
-    browserSync = require("browser-sync").create(); //only use method create
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
 
-gulp.task("watch", function () {
+gulp.task('watch', function() {
 
-    browserSync.init({
-        notify: false,
-        server: {
-            baseDir: "app" //"app" is folder, it auto detects html.
-        }
-    });
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
 
-    watch("./app/index.html", function () {
-        browserSync.reload();
-    });
+  watch('./app/index.html', function() {
+    browserSync.reload();
+  });
 
-    watch("./app/assets/styles/**/*.css", function () {
-        gulp.start("cssInject");
-    });
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('cssInject');
+  });
+
 });
 
-gulp.task("cssInject", ["style"], function () {
-    return gulp.src("app/temp/styles/style.css").pipe(browserSync.stream());
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
