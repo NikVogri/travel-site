799153d18776f66637e2670669cd69dd7ee0a354
Nik Vogrinec <vogrinec.nik@gmail.com>
2019-10-05 21:35:33 +0200

    added modal

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -2,9 +2,11 @@ import $ from 'jquery';
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from './modules/StickyHeader';
+import Modal from "./modules/Modal";
 
 
 const stickyHeader = new StickyHeader();
 const mobileMenu = new MobileMenu();
+const modal = new Modal();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
